7b1d2cda98b361d9f60c361a8532efcce021b0d2
PashaFast <pashahit@gmail.com>
2020-01-23 19:52:30 +0300

    Task is done

--- MatrixManipulations/MatrixExtension.cs
+++ MatrixManipulations/MatrixExtension.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace MatrixManipulations
 {
@@ -27,7 +27,40 @@ namespace MatrixManipulations
         /// </example>
         public static int[,] GetMatrix(int size)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (size <= 0)
+            {
+                throw new ArgumentException("Zize cannot be zero or negative.");
+            }
+
+            int[,] array = new int[size, size];
+
+            int numbers = 1;
+            int i = 0;
+            int j = 0;
+            while (numbers <= size * size)
+            {
+                array[i, j] = numbers;
+                if (i <= j + 1 && i + j < size - 1)
+                {
+                    ++j;
+                }
+                else if (i < j && i + j >= size - 1)
+                {
+                    ++i;
+                }
+                else if (i >= j && i + j > size - 1)
+                {
+                    --j;
+                }
+                else
+                {
+                    --i;
+                }
+
+                ++numbers;
+            }
+
+            return array;
         }
     }
 }
